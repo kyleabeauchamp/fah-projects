9db42e61921fbae5f6318f5258189952ce135084
Kyle Beauchamp <kyleb@mskcc-ln1.local>
2014-05-21 18:55:51 -0700

    Added new equil scripts

---
+++ code/equilibrate_boxes2.py
@@ -0,0 +1,63 @@
+import os
+import time
+from simtk.openmm import app
+import simtk.openmm as mm
+from simtk import unit as u
+import mdtraj.reporters
+import sys
+
+code = "3DMV"
+
+ff_name = "amber99sbnmr"
+water_name = 'tip3p-fb'
+
+which_forcefield = "%s.xml" % ff_name
+which_water = '%s.xml' % water_name
+
+platform_name = "CUDA"
+timestep = 2.0 * u.femtoseconds
+cutoff = 0.95 * u.nanometers
+output_frequency = 5000
+n_steps = 2500000
+temperature = 300. 
+pressure = 1.0 * u.atmospheres
+
+rank = int(sys.argv[1])
+time.sleep(rank)  # This makes sure that no two jobs run at the same time for RNG purpuses.
+
+
+pdb_filename = "./equil_box/%s.pdb" % code
+dcd_filename = "./equil_box2/%s_%d.dcd" % (code, rank)
+log_filename = "./equil_box2/%s_%d.log" % (code, rank)
+
+traj = mdtraj.load(pdb_filename)
+top, bonds = traj.top.to_dataframe()
+atom_indices = top.index[top.chainID == 0].values
+
+pdb = app.PDBFile(pdb_filename)
+topology = pdb.topology
+positions = pdb.positions
+
+ff = app.ForceField(which_forcefield, which_water)
+
+platform = mm.Platform.getPlatformByName(platform_name)
+
+system = ff.createSystem(topology, nonbondedMethod=app.PME, nonbondedCutoff=cutoff, constraints=app.HBonds)
+
+integrator = mm.LangevinIntegrator(temperature, 1.0 / u.picoseconds, timestep)
+
+simulation = app.Simulation(topology, system, integrator, platform=platform)
+simulation.context.setPositions(positions)
+
+simulation.minimizeEnergy()
+
+simulation.context.setVelocitiesToTemperature(temperature)
+
+
+print("Using platform %s" % simulation.context.getPlatform().getName())
+
+
+
+simulation.reporters.append(mdtraj.reporters.DCDReporter(dcd_filename, output_frequency))
+simulation.reporters.append(app.StateDataReporter(open(log_filename, 'w'), output_frequency, step=True, time=True, speed=True))
+simulation.step(n_steps)
