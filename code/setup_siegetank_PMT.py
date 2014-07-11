0d32e35a280345f3d1844c0a0880fddd9ee53104
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-07-11 18:44:01 -0400

    Updated PMT setup seiegetank

--- code/setup_siegetank_PMT.py
+++ code/setup_siegetank_PMT.py
@@ -3,7 +3,7 @@ import os
 import gzip
 import siegetank
 
-system_name = "NSD1_HUMAN_D0"
+system_name = "SETD2_HUMAN_D0"
 
 # Need a more secure way to store and load this.
 my_token = os.environ["SIEGETANK_TOKEN"]
@@ -11,7 +11,7 @@ siegetank.login(my_token)
 
 RUNS_PATH = "/home/kyleb/src/choderalab/FAHNVT/%s/RUN0/" % system_name
 
-opts = {'description': 'In this project we are examining the dynamics of methyltransferase NSD1, so that we might eventually develop more effective cancer therapies.', 'steps_per_frame': 125000}
+opts = {'description': 'In this project we are examining the dynamics of methyltransferase SETD2, so that we might eventually develop more effective cancer therapies.', 'steps_per_frame': 125000}
 
 target = siegetank.add_target(options=opts, engines=['openmm_601_cuda', 'openmm_601_opencl', 'openmm_601_cpu'])
 
@@ -23,7 +23,7 @@ integrator_filename = os.path.join(RUNS_PATH, "integrator.xml")
 integrator_gz = gzip.compress(open(integrator_filename, 'rb').read())
 encoded_intg = base64.b64encode(integrator_gz).decode()
 
-for i in range(100, 200):
+for i in range(200):
     print(i)
     state_filename = os.path.join(RUNS_PATH, "state%d.xml" % i)
     state_gz = gzip.compress(open(state_filename, 'rb').read())
