fe8d7b283fb1156ac7330955d079837e9c31128f
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-07-14 12:47:52 -0400

    Fixed concat_siegetank_traj.py

--- code/analysis/concat_siegetank_trajectories.py
+++ code/analysis/concat_siegetank_trajectories.py
@@ -1,13 +1,20 @@
-import numpy as np
 import os
 import glob
 import mdtraj as md
 
-trj0 = md.load("system.subset.pdb")
-filenames = glob.glob("full_Trajectories/*.h5")
+target = "NSD1_HUMAN_D0"
+target_id_map = {"NSD1_HUMAN_D0":"91f51e8a-7b40-4adc-8ca9-38786a9fe654"}
+target_id = target_id_map[target]
 
-for in_filename in filenames:
-    print(in_filename)
-    out_filename = os.path.join("./Trajectories/", os.path.split(in_filename)[1])
-    trj = md.load(in_filename, atom_indices=np.arange(trj0.n_atoms))
-    trj.save(out_filename)
+storage_path = "/home/kyleb/dat/siegetank/"
+output_directory = "/home/kyleb/dat/siegetank_analysis/%s/" % target
+
+#trj0 = md.load("/home/kyleb/src/choderalab/FAHNVT/src/equil_npt/equil_npt_final_step.pdb")
+trj0 = md.load("/home/kyleb/src/choderalab/FAHNVT/%s/oldRUN0/system.pdb" % target)
+
+streams = glob.glob(os.path.join(storage_path, target_id, "*"))
+
+for stream in streams:
+    output_filename = os.path.split(stream)[-1][0:25]
+    output_filename = os.path.join(output_directory, output_filename + ".h5")
+    md.utils.fah.concatenate_ocore(stream, trj0, output_filename)
